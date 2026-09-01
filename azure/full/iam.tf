599e50aa6440f80ea446d6ebf4d2d55a0bdecb4c
Karla <karlacrnjevic123@gmail.com>
2026-09-01 19:28:29 +0200

    Refine Azure VM RBAC assignments

--- azure/full/iam.tf
+++ azure/full/iam.tf
@@ -1,15 +1,15 @@
-resource "azurerm_role_assignment" "developer" {
+resource "azurerm_role_assignment" "developer_vm" {
   for_each = var.developer_principal_ids
 
-  scope                = azurerm_resource_group.techsprint.id
-  role_definition_name = "Reader"
+  scope                = azurerm_linux_virtual_machine.developer[each.key].id
+  role_definition_name = "Virtual Machine Contributor"
   principal_id         = each.value
 }
 
-resource "azurerm_role_assignment" "lead" {
-  count = var.lead_principal_id != "" ? 1 : 0
+resource "azurerm_role_assignment" "lead_vm" {
+  for_each = var.lead_principal_id != "" ? local.developers : {}
 
-  scope                = azurerm_resource_group.techsprint.id
-  role_definition_name = "Contributor"
+  scope                = azurerm_linux_virtual_machine.developer[each.key].id
+  role_definition_name = "Virtual Machine Contributor"
   principal_id         = var.lead_principal_id
 }
