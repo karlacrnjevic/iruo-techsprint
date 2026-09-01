c02a2bbdbbab82b48f3e2f2756d3563de397082e
Karla <karlacrnjevic123@gmail.com>
2026-09-01 19:55:38 +0200

    Use per-developer internal load balancers

--- azure/full/loadbalancer.tf
+++ azure/full/loadbalancer.tf
@@ -1,21 +1,25 @@
-resource "azurerm_lb" "techsprint" {
-  name                = "lb-techsprint-internal"
+resource "azurerm_lb" "developer" {
+  for_each = local.developers
+
+  name                = "lb-${each.key}-internal"
   location            = azurerm_resource_group.techsprint.location
   resource_group_name = azurerm_resource_group.techsprint.name
   sku                 = "Standard"
 
-  tags = local.common_tags
-
   frontend_ip_configuration {
     name                          = "internal-frontend"
-    subnet_id                     = azurerm_subnet.management.id
+    subnet_id                     = azurerm_subnet.developer[each.key].id
     private_ip_address_allocation = "Dynamic"
   }
+
+  tags = local.common_tags
 }
 
-resource "azurerm_lb_backend_address_pool" "moodle" {
-  name            = "moodle-backend-pool"
-  loadbalancer_id = azurerm_lb.techsprint.id
+resource "azurerm_lb_backend_address_pool" "developer" {
+  for_each = local.developers
+
+  name            = "backend-${each.key}-moodle"
+  loadbalancer_id = azurerm_lb.developer[each.key].id
 }
 
 resource "azurerm_network_interface_backend_address_pool_association" "developer" {
@@ -23,12 +27,14 @@ resource "azurerm_network_interface_backend_address_pool_association" "developer
 
   network_interface_id    = azurerm_network_interface.developer[each.key].id
   ip_configuration_name   = "internal"
-  backend_address_pool_id = azurerm_lb_backend_address_pool.moodle.id
+  backend_address_pool_id = azurerm_lb_backend_address_pool.developer[each.key].id
 }
 
-resource "azurerm_lb_probe" "http" {
-  name                = "http-probe"
-  loadbalancer_id     = azurerm_lb.techsprint.id
+resource "azurerm_lb_probe" "developer_http" {
+  for_each = local.developers
+
+  name                = "http-health"
+  loadbalancer_id     = azurerm_lb.developer[each.key].id
   protocol            = "Http"
   port                = 80
   request_path        = "/moodle-health.html"
@@ -36,13 +42,17 @@ resource "azurerm_lb_probe" "http" {
   number_of_probes    = 2
 }
 
-resource "azurerm_lb_rule" "http" {
-  name                           = "http-rule"
-  loadbalancer_id                = azurerm_lb.techsprint.id
+resource "azurerm_lb_rule" "developer_http" {
+  for_each = local.developers
+
+  name                           = "http"
+  loadbalancer_id                = azurerm_lb.developer[each.key].id
   protocol                       = "Tcp"
   frontend_port                  = 80
   backend_port                   = 80
   frontend_ip_configuration_name = "internal-frontend"
-  backend_address_pool_ids       = [azurerm_lb_backend_address_pool.moodle.id]
-  probe_id                       = azurerm_lb_probe.http.id
+  backend_address_pool_ids = [
+    azurerm_lb_backend_address_pool.developer[each.key].id
+  ]
+  probe_id = azurerm_lb_probe.developer_http[each.key].id
 }
