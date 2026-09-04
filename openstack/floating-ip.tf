58800948075a9403f05ee24b95c744f6eab71d43
Karla <karlacrnjevic123@gmail.com>
2026-09-04 15:42:30 +0200

    Fix OpenStack floating IP association

--- openstack/floating-ip.tf
+++ openstack/floating-ip.tf
@@ -2,7 +2,12 @@ resource "openstack_networking_floatingip_v2" "jump" {
   pool = var.external_network_name
 }
 
-resource "openstack_compute_floatingip_associate_v2" "jump" {
+data "openstack_networking_port_v2" "jump" {
+  device_id  = openstack_compute_instance_v2.jump.id
+  network_id = openstack_networking_network_v2.management.id
+}
+
+resource "openstack_networking_floatingip_associate_v2" "jump" {
   floating_ip = openstack_networking_floatingip_v2.jump.address
-  instance_id = openstack_compute_instance_v2.jump.id
+  port_id     = data.openstack_networking_port_v2.jump.id
 }
